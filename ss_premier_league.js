2a5c088979e90e25e3166dcd510713d54077da9e
lyfe05 <mastercarder.169@gmail.com>
2025-08-13 01:15:58 +0300

    Create ss_premier_league.js

---
+++ ss_premier_league.js
@@ -0,0 +1,5 @@
+#EXTINF:-1 group-title="SOUTH AFRICA" tvg-id="test" tvg-logo="https://raw.githubusercontent.com/nightah/daddylive/refs/heads/main/logos/supersport-premier-league.png",SuperSport Premier league
+#EXTVLCOPT:http-origin=https://jxoplay.xyz
+#EXTVLCOPT:http-referrer=https://jxoplay.xyz/
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36
+https://ddy6new.newkso.ru/ddy6/premium414/mono.m3u8
